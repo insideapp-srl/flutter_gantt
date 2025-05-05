ccb37e31fbcf85381fedbe7726705582f5d8a965
riccardocucia <r.cucia@insideapp.it>
2025-05-05 11:52:03 +0200

    Wip invisible cell

--- lib/src/widgets/row.dart
+++ lib/src/widgets/row.dart
@@ -25,69 +25,75 @@ class GanttActivityRow extends StatelessWidget {
         (context, child) => SizedBox(
           height: context.watch<GanttTheme>().cellHeight,
           child:
-              context.watch<GanttActivityCtrl>().cellVisible && activity.showCell
-                  ? Row(
-                    children: [
-                      Expanded(
-                        flex: context.watch<GanttActivityCtrl>().cellsFlexStart,
-                        child: Container(),
-                      ),
-                      Expanded(
-                        flex: context.watch<GanttActivityCtrl>().cellsFlex,
+              activity.showCell
+                  ? (context.watch<GanttActivityCtrl>().cellVisible
+                      ? Row(
+                        children: [
+                          Expanded(
+                            flex:
+                                context
+                                    .watch<GanttActivityCtrl>()
+                                    .cellsFlexStart,
+                            child: Container(),
+                          ),
+                          Expanded(
+                            flex: context.watch<GanttActivityCtrl>().cellsFlex,
+                            child: Tooltip(
+                              message: activity.description,
+                              child: GanttCell(activity: activity),
+                            ),
+                          ),
+                          Expanded(
+                            flex:
+                                context.watch<GanttActivityCtrl>().cellsFlexEnd,
+                            child: Container(),
+                          ),
+                        ],
+                      )
+                      : context.watch<GanttActivityCtrl>().showBefore
+                      ? Align(
+                        alignment: Alignment.centerLeft,
                         child: Tooltip(
-                          message: activity.description,
-                          child: GanttCell(activity: activity),
+                          message:
+                              '${activity.start.toLocal()} - ${activity.end.toLocal()}',
+                          child: InkWell(
+                            onTap:
+                                () =>
+                                    context.read<GanttController>().startDate =
+                                        activity.start,
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Icon(Icons.navigate_before),
+                                Text(activity.title),
+                              ],
+                            ),
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        flex: context.watch<GanttActivityCtrl>().cellsFlexEnd,
-                        child: Container(),
-                      ),
-                    ],
-                  )
-                  : context.watch<GanttActivityCtrl>().showBefore
-                  ? Align(
-                    alignment: Alignment.centerLeft,
-                    child: Tooltip(
-                      message:
-                          '${activity.start.toLocal()} - ${activity.end.toLocal()}',
-                      child: InkWell(
-                        onTap:
-                            () =>
-                                context.read<GanttController>().startDate =
-                                    activity.start,
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Icon(Icons.navigate_before),
-                            Text(activity.title),
-                          ],
+                      )
+                      : Align(
+                        alignment: Alignment.centerRight,
+                        child: Tooltip(
+                          message:
+                              '${activity.start.toLocal()} - ${activity.end.toLocal()}',
+                          child: InkWell(
+                            onTap:
+                                () =>
+                                    context.read<GanttController>().startDate =
+                                        activity.start,
+                            child: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(activity.title),
+                                Icon(Icons.navigate_next),
+                              ],
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
-                  )
-                  : Align(
-                    alignment: Alignment.centerRight,
-                    child: Tooltip(
-                      message:
-                          '${activity.start.toLocal()} - ${activity.end.toLocal()}',
-                      child: InkWell(
-                        onTap:
-                            () =>
-                                context.read<GanttController>().startDate =
-                                    activity.start,
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            Text(activity.title),
-                            Icon(Icons.navigate_next),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                      ))
+                  : Container(),
         ),
   );
 }
