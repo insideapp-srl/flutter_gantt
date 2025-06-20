53aff840a139c165c9a5ae8216c5857304c79652
Riccardo Baldassa <r.baldassa@insideapp.net>
2025-06-20 10:02:39 +0200

    fix holidays - @RichiB20

--- lib/src/widgets/gantt.dart
+++ lib/src/widgets/gantt.dart
@@ -188,7 +188,7 @@ class _GanttState extends State<Gantt> {
                             Positioned.fill(
                               child: Container(color: theme.backgroundColor),
                             ),
-                            CalendarGrid(holidays: widget.holidays),
+                            CalendarGrid(holidays: c.holidays),
                             ActivitiesGrid(
                               activities: c.activities,
                               controller: _gridColumnsController,
