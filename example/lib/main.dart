902079512afb7b150d5ade23bbf85b60f5b3f974
riccardocucia <r.cucia@insideapp.it>
2025-11-16 18:28:17 +0100

    Customizable flex ratios for activities list and grid area @r1wtn

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -223,7 +223,7 @@ class _MyHomePageState extends State<MyHomePage> {
     ];
   }
 
-  void _onActivityChanged(activity, DateTime? start, DateTime? end) {
+  void _onActivityChanged(GanttActivity activity, DateTime? start, DateTime? end) {
     if (start != null && end != null) {
       debugPrint('$activity was moved (Event on controller)');
     } else if (start != null) {
